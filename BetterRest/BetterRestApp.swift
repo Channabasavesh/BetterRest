fdeba02727c8c9abeb1de87c3d9e0b5c10a1d8aa
Channabasavesh Chandrappa Budihal <cbudihal@apple.com>
2021-02-01 23:11:42 +0530

    Initial Commit
    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Channabasavesh Chandrappa Budihal on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
